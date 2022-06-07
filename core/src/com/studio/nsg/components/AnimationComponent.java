8ba1c9ed438111de98b4936ee6705df989ff7378
Resst <gamov.ia@edu.spbstu.ru>
2022-06-07 17:07:44 +0300

    Adding AnimationComponent

---
+++ core/src/com/studio/nsg/components/AnimationComponent.java
@@ -0,0 +1,9 @@
+package com.studio.nsg.components;
+
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.IntMap;
+
+public class AnimationComponent {
+    public IntMap<Animation<TextureRegion>> animation;
+}
