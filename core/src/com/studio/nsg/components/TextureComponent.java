214a10777a940208666b3426d5ff433be370ccb3
Resst <gamov.ia@edu.spbstu.ru>
2022-06-07 17:04:20 +0300

    Adding TextureComponent

---
+++ core/src/com/studio/nsg/components/TextureComponent.java
@@ -0,0 +1,9 @@
+package com.studio.nsg.components;
+
+import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+public class TextureComponent implements Component {
+    public Sprite sprite;
+    public int priority;
+}
