de9c6e1be637c0cf7d3745ab3a9cdf78acc9a5cf
Resst <gamov.ia@edu.spbstu.ru>
2022-06-07 17:09:14 +0300

    Adding CollisionComponent

---
+++ core/src/com/studio/nsg/components/CollisionComponent.java
@@ -0,0 +1,8 @@
+package com.studio.nsg.components;
+
+import com.badlogic.ashley.core.Component;
+import com.badlogic.ashley.core.Entity;
+
+public class CollisionComponent implements Component {
+    public Entity collided;
+}
