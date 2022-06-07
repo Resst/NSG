c9357c44c38698e6a154cc40a50f6a16217a0e71
Resst <gamov.ia@edu.spbstu.ru>
2022-06-07 17:28:01 +0300

    Adding Component Implementation to AnimationComponent

--- core/src/com/studio/nsg/components/AnimationComponent.java
+++ core/src/com/studio/nsg/components/AnimationComponent.java
@@ -1,9 +1,10 @@
 package com.studio.nsg.components;
 
+import com.badlogic.ashley.core.Component;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.IntMap;
 
-public class AnimationComponent {
+public class AnimationComponent implements Component {
     public IntMap<Animation<TextureRegion>> animation;
 }
