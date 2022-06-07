53c23f4d3e0a5d7dda04dd65374c196ee6f326b2
Resst <gamov.ia@edu.spbstu.ru>
2022-06-07 17:16:27 +0300

    Adding Stat Component

---
+++ core/src/com/studio/nsg/components/EntityStatComponent.java
@@ -0,0 +1,8 @@
+package com.studio.nsg.components;
+
+import com.badlogic.ashley.core.Component;
+
+public class EntityStatComponent implements Component {
+    public int hp;
+    public int armor;
+}
