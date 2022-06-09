6d6fb5e367c01f4ce91b1936441e6a72a0c2762d
Akamoto529 <40491944+Akamoto529@users.noreply.github.com>
2022-06-09 19:04:35 +0300

    Добавлена загрузка .tmx карт

---
+++ assets/Maps/Test/Test Tileset.tsx
@@ -0,0 +1,68 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="Test Tileset" tilewidth="16" tileheight="16" tilecount="72" columns="6">
+ <image source="TestTiles.png" width="96" height="192"/>
+ <tile id="24">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="2" y="7" width="13" height="9"/>
+  </objectgroup>
+ </tile>
+ <tile id="25">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="2.67943" y="16.2892">
+    <polygon points="0,0 0.170122,0.0850611 0.127592,0.0425306"/>
+   </object>
+   <object id="5" x="2" y="3" width="14" height="13"/>
+   <object id="6" x="2.93567" y="3" width="13.0643" height="13"/>
+  </objectgroup>
+ </tile>
+ <tile id="26">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="3" width="16" height="13"/>
+  </objectgroup>
+ </tile>
+ <tile id="27">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="2" width="13.9793" height="14"/>
+  </objectgroup>
+ </tile>
+ <tile id="30">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="0" width="15" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="31">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1.93567" y="0" width="14.0643" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="33">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="36">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="0" width="15" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="37">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3.04147" y="0" width="12.9585" height="15.0218"/>
+  </objectgroup>
+ </tile>
+ <tile id="38">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="14.9793"/>
+  </objectgroup>
+ </tile>
+ <tile id="39">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="14.9793" height="15.0218"/>
+  </objectgroup>
+ </tile>
+ <tile id="42">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="2.9782" y="2" width="11.0218" height="11.9585"/>
+  </objectgroup>
+ </tile>
+</tileset>
--- core/src/com/studio/nsg/Tools/ResourceManager.java
+++ core/src/com/studio/nsg/Tools/ResourceManager.java
@@ -5,29 +5,38 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 
 public class ResourceManager {
-    //Папка, в которой хранятся атласы с текстурами
-    private static final String ATLAS_FOLDER = "Atlas_Tests/Atlas/";
-    //Список всех атласов (потом можно будет автоматизировать (или нет, их много не будет (скорее всего будет только один)))
-    private static final String[] ATLAS_NAMES = {"Test.Atlas"};
-    private AssetManager assetManager;
+    private static final String MAPS_FOLDER = "Maps/";
+    private static final String ATLAS_FOLDER = "Atlases/";
+    private static final String[] ATLAS_NAMES = {"Test"};
+    private static final String[] MAP_NAMES = {"Test"};
+    private final AssetManager assetManager;
     public ResourceManager(){
         assetManager = new AssetManager();
+        assetManager.setLoader(TiledMap.class, new TmxMapLoader());
         for (String atlas_name:
              ATLAS_NAMES) {
-            assetManager.load(ATLAS_FOLDER + atlas_name, TextureAtlas.class);
+            assetManager.load(ATLAS_FOLDER + atlas_name + "/" + atlas_name + ".atlas", TextureAtlas.class);
+        }
+        for (String map_name:
+                MAP_NAMES) {
+            assetManager.load(MAPS_FOLDER + map_name + "/" + map_name + ".tmx", TiledMap.class);
         }
         assetManager.finishLoading();
     }
-    public Animation getAnimation(String entity_name, String animation_type, float frameDuration, Animation.PlayMode mode)
-    {
-        TextureAtlas atlas = assetManager.get(ATLAS_FOLDER + "Test.atlas",TextureAtlas.class);
-       return new Animation<TextureRegion>(frameDuration,atlas.findRegions(entity_name + "/" + animation_type+ "/" + animation_type), mode);
+    public Animation<TextureRegion> getAnimation(String entity_name, String animation_type, float frameDuration, Animation.PlayMode mode) {
+        TextureAtlas atlas = assetManager.get(ATLAS_FOLDER + ATLAS_NAMES[0] + "/" + ATLAS_NAMES[0] + ".atlas",TextureAtlas.class);
+       return new Animation<>(frameDuration,atlas.findRegions(entity_name + "/" + animation_type+ "/" + animation_type), mode);
     }
-    public Texture getTexture(String Entity_name){
-        TextureAtlas atlas = assetManager.get(ATLAS_FOLDER + "Test.atlas",TextureAtlas.class);
-        return atlas.findRegion(Entity_name).getTexture();
+    public Texture getTexture(String entity_name){
+        TextureAtlas atlas = assetManager.get(ATLAS_FOLDER + ATLAS_NAMES[0]+".atlas",TextureAtlas.class);
+        return atlas.findRegion(entity_name).getTexture();
+    }
+    public TiledMap getMap(String map_name){
+        return assetManager.get(MAPS_FOLDER + map_name + "/" + map_name + ".tmx", TiledMap.class);
     }
     public void dispose(){
         assetManager.dispose();
